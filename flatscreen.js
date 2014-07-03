47fc0226df59ff201ba0a60f6dc4b939d063b8e0
Ian James <ijames@wwf.org.uk>
2014-07-03 22:37:29 +0100

    Fixed alternative thumbnails.

--- flatscreen.js
+++ flatscreen.js
@@ -179,18 +179,18 @@ function flatscreen(youtubeParameters, gaEvents, alternativeThumbnails){
             // If the thumbnails have been set, the use them
             // -----------------------------------------------------------------
             if ( typeof alternativeThumbnails === undefined ) {
-                // console.log('alternative thumbnail - ' + alternativeThumbnails[ytID]);
-
-                var invisibleButton = '<div title="Click to play" class="invisible-button" id="' + ytID + '-invisible-button"><img src="' + alternativeThumbnails[ytID] + '" /></div>',
-                loadingScreen = '<div class="loading-screen" id="' + ytID + '-loading"><div class="loading-spinner" id="' + ytID + '-spinner"><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span></div><img src="' + alternativeThumbnails[ytID] + '" /></div>';
+                var invisibleButton = '<div title="Click to play" class="invisible-button" id="' + ytID + '-invisible-button"><img src="//i1.ytimg.com/vi/' + ytID + '/mqdefault.jpg" /></div>',
+                loadingScreen = '<div class="loading-screen" id="' + ytID + '-loading"><div class="loading-spinner" id="' + ytID + '-spinner"><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span></div><img src="//i1.ytimg.com/vi/' + ytID + '/hqdefault.jpg" /></div>';
                 // console.log(screens[i].id);
             }
 
             //
             // -----------------------------------------------------------------
             else {
-                var invisibleButton = '<div title="Click to play" class="invisible-button" id="' + ytID + '-invisible-button"><img src="//i1.ytimg.com/vi/' + ytID + '/mqdefault.jpg" /></div>',
-                loadingScreen = '<div class="loading-screen" id="' + ytID + '-loading"><div class="loading-spinner" id="' + ytID + '-spinner"><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span></div><img src="//i1.ytimg.com/vi/' + ytID + '/hqdefault.jpg" /></div>';
+                // console.log('alternative thumbnail - ' + alternativeThumbnails[ytID]);
+
+                var invisibleButton = '<div title="Click to play" class="invisible-button" id="' + ytID + '-invisible-button"><img src="' + alternativeThumbnails[ytID] + '" /></div>',
+                loadingScreen = '<div class="loading-screen" id="' + ytID + '-loading"><div class="loading-spinner" id="' + ytID + '-spinner"><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span><span class="circles"></span></div><img src="' + alternativeThumbnails[ytID] + '" /></div>';
                 // console.log(screens[i].id);
             }
 
